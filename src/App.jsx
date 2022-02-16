1a63fba0e56f674ac6896bdeee387d4033cf8e29
luksfay <lukspicco@hotmail.com>
2022-02-16 09:31:58 +0000

    commit before sleeping

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.scss';
 import Navbar from './components/navbar/Navbar';
 import Intro from './components/intro/Intro';
 import Portfolio from './components/portfolio/Portfolio';
@@ -19,12 +19,6 @@ const App = ()=>{
     		<Contact />
     			
     		</div>
-    		section (
-    			-intro
-    		  -portfolio 
-    		  -work 
-    		  -contact
-    		  ) 
     	</div>
     </>
   );
