31fb7dd35682b012f218571a53f4d2ed887f0ff7
luksfay <lukspicco@hotmail.com>
2022-02-16 09:20:09 +0000

    adding the other components for preview

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import './App.css';
 import Navbar from './components/navbar/Navbar';
+import Intro from './components/intro/Intro';
+import Portfolio from './components/portfolio/Portfolio';
+import Works from './components/works/Works';
+import Testimonials from './components/testimonials/Testimonials';
+import Contact from './components/contact/Contact';
 
 const App = ()=>{
   return (
     <>
     	<div className='app'>
     		<Navbar />
+    		<div className='sections'>
+    		<Intro />
+    		<Portfolio />
+    		<Works/>
+    		<Testimonials/>
+    		<Contact />
+    			
+    		</div>
     		section (
     			-intro
     		  -portfolio 
