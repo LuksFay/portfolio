010aea8109f0f08ed585eea11fb00a8dc9dfaf62
luksfay <lukspicco@hotmail.com>
2022-02-18 06:57:01 +0000

    learning on my way, react-fontawesome

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,13 +4,33 @@ const Navbar = () => {
 	return(
 		<>
 			<div className='navbar'> 
+			
 				<div className='wrapper'>
+				
 					<div className='left'>
-						<a href='#intro'>intro</a>
+					
+						<a href='#intro' className='logo'>
+							Portfolio.
+						</a>
+						
+						<div className='itemContainer'>
+							<p className='icon'>Logo</p>
+							<button className='github-button'> Check out my Github!</button>	
+						</div>
+						
+						<div className='itemContainer'>
+							<p className='icon'>Logo</p>
+							<button className='twitter-button'>Here's my Twitter, follow me!</button>	
+						</div>
+						
 					</div>
 					
-					<div className='right'></div>
+					<div className='right'>
+					
+					</div>
+					
 				</div>
+				
 			</div>
 		</>
 	);
