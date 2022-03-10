3dd7fec838ab02c8d86927ae44f612b29d83935b
luksfay <lukspicco@hotmail.com>
2022-03-10 04:33:08 +0000

    introducing: me

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -4,6 +4,12 @@ const Intro = () => {
 	return(
 		<>
 			<div className='intro' id='intro'>
+				<div className='left'>
+					<div className='imgContainer'>
+						<img src='assets/man.png' alt=''/>
+					</div>
+				</div>
+				<div className='right'></div>
 			</div>
 		</>
 	);
