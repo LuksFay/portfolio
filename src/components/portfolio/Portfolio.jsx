fa2154be38de1e21b6a36f5de94f897f9b0e720b
luksfay <lukspicco@hotmail.com>
2022-03-10 23:17:42 +0000

    syntax error in importation

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,6 +1,6 @@
 import {useState} from 'react';
 import './portfolio.scss';
-import PortfolioList from './components/portfolioList/PortfolioList';
+import PortfolioList from './portfolioList/PortfolioList';
 
 
 const Portfolio = () => {
