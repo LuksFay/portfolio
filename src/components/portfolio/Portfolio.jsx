6af5461c8ab3ac08a57a1d9dc233cd634c310af0
luksfay <lukspicco@hotmail.com>
2022-03-10 22:57:47 +0000

    example pics for the portfolio, dinamic selection-bar

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,69 @@
+import {useState} from 'react';
 import './portfolio.scss'
 
 const Portfolio = () => {
+
+	const [selected, setSelected] = useState('featured') 
+	const list = [
+		{
+			id: 'featured',
+			title: 'Featured'
+		},
+		{
+			id: 'web',
+			title: 'Web App'
+		},
+		{
+			id: 'mobile',
+			title: 'Mobile App'
+		},
+		{
+			id: 'design',
+			title: 'Design'
+		},
+		{
+			id: 'branding',
+			title: 'Branding'
+		}
+	] 
+
 	return(
 		<>
-			<div className='portfolio' id='portfolio'></div>
+			<div className='portfolio' id='portfolio'>
+				
+				<h1>Portfolio</h1>
+				
+				<ul>
+					{list.map(item=>(
+						<PortfolioList 
+							title={item.title}
+							active={selected === item.id}
+							setSelected={setSelected}
+							id= {item.id}
+						/>
+					))}
+				</ul>
+			
+				<div className='container'>
+					
+					<div className='item'>
+						<img src='assets/example.png' alt=''/>
+						<h3>Banking App</h3>
+					</div>
+					
+					<div className='item'>
+						<img src='assets/example.png' alt=''/>
+						<h3>E-Commerce App</h3>
+					</div>
+					
+					<div className='item'>
+						<img src='assets/example.png' alt=''/>
+						<h3>Game App</h3>
+					</div>
+					
+				</div>
+				
+			</div>
 		</>
 	);
 };
---
+++ src/components/portfolioList/PortfolioList.jsx
@@ -0,0 +1,16 @@
+import './portfolioList.scss';
+
+const PortfolioList = ({id, title, active, setSelected}) => {
+	return(
+		<>
+			<li
+			 className={active ? 'portfolioList active' : 'portfolioList'} 
+			 onClick={()=>setSelected(id)}
+			>
+				{title}
+			</li>
+		</>
+	);
+};
+
+export default PortfolioList;
