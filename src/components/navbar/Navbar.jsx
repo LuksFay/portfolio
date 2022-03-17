e48d3aedf52b4ba5884f636497445e49d9dd03d7
Lucas Picco <lukspicco@hotmail.com>
2022-03-17 17:36:50 -0300

    link buttons

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -19,13 +19,17 @@ const Navbar = ({menuOpen, setMenuOpen}) => {
 						</a>
 						
 						<div className='itemContainer github-button'>
-								<FontAwesomeIcon icon='fa-brands fa-github icon' /> 
-						 		<span>Check out my Github!</span>
+								<a href='https://github.com/LuksFay'>
+									<FontAwesomeIcon icon='fa-brands fa-github icon' className='icon' /> 
+						 			<span className='span'>Check out my Github!</span>
+								</a>
 						</div>
 						
 						<div className='itemContainer twitter-button'>
-								<FontAwesomeIcon icon='fa-brands fa-twitter icon' className='icon'/> 
-								<span className='span' >Here's my Twitter, follow me!</span>
+								<a href='https://twitter.com/LuksfayDev'>
+									<FontAwesomeIcon icon='fa-brands fa-twitter icon' className='icon'/> 
+									<span className='span' >Here's my Twitter, follow me!</span>
+								</a>
 						</div>
 						
 					</div>
